2179256f074cecd350721b5ba7fd3cc10d135bbb
Matt Starfield <matt.starfield@nextfab.com>
2021-05-03 19:56:22 -0400

    * updated audio example * converted wavs to mp3s

--- firmware/arduino_ide/audio_example.ino/audio_example.ino.ino
+++ firmware/arduino_ide/audio_example.ino/audio_example.ino.ino
@@ -6,19 +6,19 @@
 #include <Adafruit_VS1053.h>
 
 // These are the pins used
-#define VS1053_RESET   -1     // VS1053 reset pin (not used!)
+#define PIN_VS1053_RESET   -1     // VS1053 reset pin (not used!)
 
 // Feather M4, M0, 328, nRF52840 or 32u4
-  #define VS1053_CS       6     // VS1053 chip select pin (output)
-  #define VS1053_DCS     10     // VS1053 Data/command select pin (output)
-  #define CARDCS          5     // Card chip select pin
+  #define PIN_SPI_VS1053_CS       6     // VS1053 chip select pin (output)
+  #define PIN_VS1053_DSEL     10     // VS1053 Data/command select pin (output)
+  #define PIN_SPI_SD_CS          5     // Card chip select pin
   // DREQ should be an Int pin *if possible* (not possible on 32u4)
-  #define VS1053_DREQ     9     // VS1053 Data request, ideally an Interrupt pin
+  #define PIN_VS1053_DREQ     9     // VS1053 Data request, ideally an Interrupt pin
 
 
 
-Adafruit_VS1053_FilePlayer musicPlayer = 
-  Adafruit_VS1053_FilePlayer(VS1053_RESET, VS1053_CS, VS1053_DCS, VS1053_DREQ, CARDCS);
+Adafruit_VS1053_FilePlayer audioPlayer = 
+  Adafruit_VS1053_FilePlayer(PIN_VS1053_RESET, PIN_SPI_VS1053_CS, PIN_VS1053_DSEL, PIN_VS1053_DREQ, PIN_SPI_SD_CS);
 
 void setup() {
   Serial.begin(115200);
@@ -31,16 +31,16 @@ void setup() {
   delay(500);
   Serial.println("\n\nAdafruit VS1053 Feather Test");
   
-  if (! musicPlayer.begin()) { // initialise the music player
+  if (! audioPlayer.begin()) { // initialise the music player
      Serial.println(F("Couldn't find VS1053, do you have the right pins defined?"));
      while (1);
   }
 
   Serial.println(F("VS1053 found"));
  
-  //musicPlayer.sineTest(0x44, 500);    // Make a tone to indicate VS1053 is working
+  //audioPlayer.sineTest(0x44, 500);    // Make a tone to indicate VS1053 is working
   
-  if (!SD.begin(CARDCS)) {
+  if (!SD.begin(PIN_SPI_SD_CS)) {
     Serial.println(F("SD failed, or not present"));
     while (1);  // don't do anything more
   }
@@ -50,29 +50,42 @@ void setup() {
   printDirectory(SD.open("/"), 0);
   
   // Set volume for left, right channels. lower numbers == louder volume!
-  //musicPlayer.setVolume(10,10);
-  musicPlayer.setVolume(2,2);
+  //audioPlayer.setVolume(10,10);
+  audioPlayer.setVolume(2,2);
   
   // If DREQ is on an interrupt pin we can do background
   // audio playing
-  musicPlayer.useInterrupt(VS1053_FILEPLAYER_PIN_INT);  // DREQ int
+  audioPlayer.useInterrupt(VS1053_FILEPLAYER_PIN_INT);  // DREQ int
 
   
   // Play a file in the background, REQUIRES interrupts!
-  Serial.println(F("Playing STARTUP1.MP3"));
-  musicPlayer.playFullFile("/STARTUP1.MP3");
+  Serial.println(F("Playing startup1.mp3"));
+  audioPlayer.playFullFile("/startup1.mp3");
+  //audioPlayer.startPlayingFile("/startup1.mp3");
 
-  Serial.println(F("Playing COUNT.WAV"));
-  musicPlayer.startPlayingFile("COUNT.WAV");
+  Serial.println(F("Playing countdwn.mp3"));
+  audioPlayer.playFullFile("/countdwn.mp3");
 
-  Serial.println(F("Playing BEGIN.WAV"));
-  musicPlayer.startPlayingFile("BEGIN.WAV");
+  Serial.println(F("Playing begin001.mp3"));
+  audioPlayer.playFullFile("/begin001.mp3");
+
+  Serial.println(F("Playing alert001.mp3"));
+  audioPlayer.playFullFile("/alert001.mp3");
+
+  Serial.println(F("Playing alert002.mp3"));
+  audioPlayer.playFullFile("/alert002.mp3");
+
+  Serial.println(F("Playing alert003.mp3"));
+  audioPlayer.playFullFile("/alert003.mp3");
+
+  Serial.println(F("Playing alert004.mp3"));
+  audioPlayer.playFullFile("/alert004.mp3");
 }
 
 void loop() {
   Serial.print(".");
   // File is playing in the background
-  if (musicPlayer.stopped()) {
+  if (audioPlayer.stopped()) {
     Serial.println("Done playing music");
     while (1) {
       delay(10);  // we're done! do nothing...
@@ -83,17 +96,17 @@ void loop() {
     
     // if we get an 's' on the serial console, stop!
     if (c == 's') {
-      musicPlayer.stopPlaying();
+      audioPlayer.stopPlaying();
     }
     
     // if we get an 'p' on the serial console, pause/unpause!
     if (c == 'p') {
-      if (! musicPlayer.paused()) {
+      if (! audioPlayer.paused()) {
         Serial.println("Paused");
-        musicPlayer.pausePlaying(true);
+        audioPlayer.pausePlaying(true);
       } else { 
         Serial.println("Resumed");
-        musicPlayer.pausePlaying(false);
+        audioPlayer.pausePlaying(false);
       }
     }
   }
